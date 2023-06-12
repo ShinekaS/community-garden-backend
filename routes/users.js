6e578ca0381e2edbf682647b2234df127a33d107
Pita Badila <ritualmomma@gmail.com>
2023-06-12 13:27:09 -0400

    Adds users model and routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const { User } = require('../models')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+//View all the users
+router.get('/',async (req,res) => {
+  try{
+      const users = await User.findAll();
+      res.json(users);
+  } catch (error) {
+      res.status(500).json({message: "Error retrieving users", error})
+  }
+  
+})
+
+//Add a new user
+router.post('/', async (req,res) => {
+  const { firstName, lastName, userName, password, phoneNumber, city, state, zipcode, email } = req.body
+  try{
+    const user = await User.create({ firstName, lastName, userName, password, phoneNumber, city, state, zipcode, email })
+    res.status(201).json(user)
+  } catch (error) {
+    res.status(500).json({message: "Error creating user"})
+  }
+})
+
+
 
 module.exports = router;
